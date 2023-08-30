07a483ac453225eadc337566a8fcbf0a6bd3bee8
Ibnu Tuharea <ibent95tuny@gmail.com>
2023-08-30 21:12:01 +0700

    Fixed tailwind columns in config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{html,ts,tsx,jsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    'col-span-1', 'col-span-2', 'col-span-3', 'col-span-4', 'col-span-5', 'col-span-6', 'col-span-7', 'col-span-8', 'col-span-9', 'col-span-10', 'col-span-11', 'col-span-12',
+    'row-span-1', 'row-span-2', 'row-span-3', 'row-span-4', 'row-span-5', 'row-span-6',
+  ],
 }
