d0fc58784957b87a3a5df6cf261be16ea2c18f87
Ibnu Tuharea <ibent95tuny@gmail.com>
2023-09-01 23:36:48 +0700

    Set the recursive logic to default grid system

--- src/app/modules/publication/publication-form/grid-systems/publication-form-grid-systems-default/publication-form-grid-systems-default.component.ts
+++ src/app/modules/publication/publication-form/grid-systems/publication-form-grid-systems-default/publication-form-grid-systems-default.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormGroup, FormGroupDirective, UntypedFormArray } from '@angular/forms';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-publication-form-grid-systems-default',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./publication-form-grid-systems-default.component.scss']
 })
 export class PublicationFormGridSystemsDefaultComponent {
+
+  @Input() available!: any;
+  @Input() forms!: FormGroup;
+  @Input() selectOptions!: any;
+  @Input() forms_metadata!: Array<any>;
+  @Input() uniqueFalseCheckField!: any;
+
+  @Output() onPublicationTypeSelected: EventEmitter<any>;
+
+  formGroup!: FormGroup;
+  publicationTypeUuid!: string;
+  publicationTypeCode!: string;
+
+  constructor(
+    private parentFormGroup: FormGroupDirective,
+    private appSvc: AppService,
+  ) {
+    this.formGroup = this.parentFormGroup.form as FormGroup;
+  }
+
+  // Function to get Form Group of formBuilder
+  public getFormGroup(fieldName: string) {
+    return this.forms?.get(fieldName) as UntypedFormArray;
+  }
+
+  public onPublicationTypeSlctSelect(data: any) {
+
+  }
 
 }
