62e77a8f355171f8cd0b744ceb238ee4fe5ffae7
Ibnu Tuharea <ibent95tuny@gmail.com>
2023-09-01 12:42:08 +0700

    Configure DF Owl Datetime components

--- src/app/enums/df-field.ts
+++ src/app/enums/df-field.ts
@@ -3,3 +3,14 @@ export enum DFFieldAppearance {
 	'fill',
 	'outline',
 }
+
+export enum DFFieldOwlDatetimeMode {
+	DATE = "date",
+	MONTH = "month",
+	YEAR = "year",
+	TIME = "time",
+	DATE_TIME = "date-time",
+	DATE_RANGE = "date-range",
+	TIME_RANGE = "time-range",
+	DATE_TIME_RANGE = "date-time-range",
+}
--- src/app/shared/dynamic-form/dynamic-form.component.ts
+++ src/app/shared/dynamic-form/dynamic-form.component.ts
@@ -3,12 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dynamic-form',
   template: `
-    <p>
-      dynamic-form works!
-    </p>
+    <p>Dynamic Form (DF)</p>
   `,
   styleUrls: ['./dynamic-form.component.scss']
 })
-export class DynamicFormComponent {
-
-}
+export class DynamicFormComponent { }
