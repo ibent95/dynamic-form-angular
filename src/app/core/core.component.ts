f8c25fac9427715582d0235c9bb9c6957ac1086e
ibent95 <ibent95tuny@gmail.com>
2023-09-23 21:59:46 +0700

    Add behaviour to close and open sidebar for mobile version

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,23 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
 	selector: 'app-core',
-	templateUrl: './core.component.html',
-	styleUrls: ['./core.component.scss']
+	template: `
+		<ng-container>
+			<mat-drawer-container class="drawer-container">
+				<mat-drawer mode="side">
+					<app-sidebar [routeMap]="routeMap" [isSidebarOpen]="isSidebarOpen" (sidebarCloseToggleClick)="onSidebarCloseToggleClick($event)"></app-sidebar>
+				</mat-drawer>
+
+				<mat-drawer-content>
+					<app-header [routeMap]="routeMap" [isSidebarOpen]="isSidebarOpen" (sidebarToggleClick)="onHeaderSidebarToggleClick($event)"></app-header>
+
+					<app-content [routeMap]="routeMap"></app-content>
+
+					<app-footer [routeMap]="routeMap"></app-footer>
+				</mat-drawer-content>
+			</mat-drawer-container>
+		</ng-container>
+	`,
+	styles: ['.drawer-container { height: 100%; width: 100%; }']
 })
-export class CoreComponent implements OnInit {
+export class CoreComponent implements OnInit, AfterViewInit {
 	routeMap!: string[];
+	isSidebarOpen: boolean;
+
+	@ViewChild(MatDrawer) sidebar!: MatDrawer;
 
 	constructor(
-		public router: Router
-	) { }
+		public router: Router,
+		private cdr: ChangeDetectorRef,
+		private breakpointObserver: BreakpointObserver,
+	) {
+		this.isSidebarOpen = true;
+	}
 
 	ngOnInit(): void {
 		this.routeMap = this.router.url.split('/');
 		this.routeMap.shift(); // To remove empty string in first object
 
 		this.subscribeRouterEvents();
+	}
+
+	ngAfterViewInit() {
+
+		// Check if display width is small or mobile version, so it can change isSideBarOpen
+		this.breakpointObserver.observe(["(max-width: 800px)"]).subscribe((response: any) => {
+			this.isSidebarOpen = (!response.matches);
+
+			if (this.isSidebarOpen) {
+				this.sidebar.open();
+			} else {
+				this.sidebar.close();
+			}
+		});
+
+		this.cdr.detectChanges();
 	}
 
 	private subscribeRouterEvents() {
@@ -27,6 +68,16 @@ export class CoreComponent implements OnInit {
 				this.routeMap.shift(); // To remove empty string in first object
 			}
 		});
+	}
+
+	public onSidebarCloseToggleClick(data: boolean) {
+		this.isSidebarOpen = data;
+		this.sidebar.close();
+	}
+
+	public onHeaderSidebarToggleClick(data: boolean) {
+		this.isSidebarOpen = data;
+		this.sidebar.open();
 	}
 
 }
