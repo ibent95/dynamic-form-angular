92ae038c3b265613371158db0ca42e5426b41ef3
Ibnu Tuharea <ibent95tuny@gmail.com>
2024-08-18 12:19:38 +0700

    Update libraries

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8080/public/api',
-  // apiUrl: 'https://svc-dynamic-form-symfony.loc/api',
+  //apiUrl: 'http://127.0.0.1:8000/public/api',
+   apiUrl: 'https://svc-dynamic-form-symfony.loc/api',
   // apiUrl: 'https://service.ibent95.my.id/svc-dynamic-form-symfony/public/api'
   ngxDocViewer: 'url',
 };
