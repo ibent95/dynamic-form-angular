40e5bf8233d6bad579d0a3d91ce679e4afeafbc3
Ibnu Tuharea <ibent95tuny@gmail.com>
2024-08-29 00:41:00 +0700

    Fixed: Polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,8 +1,10 @@
 /***************************************************************************************************
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
  */
-import '@angular/localize/init';
+
 /**
+ * import '@angular/localize/init'; // Already in angular.json - polyfills property
+ *
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
  *
