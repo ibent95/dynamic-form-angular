7d5044bcc4691072d7613ea9fc84f82f290fd832
ibent95 <ibent95tuny@gmail.com>
2024-12-20 14:05:54 +0700

    Fixed: Production build configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
   apiUrl: 'https://services.ibent95.my.id/svc-dynamic-form-symfony/public/api',
+  symfonyApiUrl: 'https://services.ibent95.my.id/svc-dynamic-form-symfony/public/api',
+  lumenApiUrl: 'https://services.ibent95.my.id/svc-dynamic-form-lumen/public/api',
+  springApiUrl: 'https://services.ibent95.my.id/svc-dynamic-form-spring/public/api',
+  micronautApiUrl: 'https://services.ibent95.my.id/svc-dynamic-form-micronaut/public/api',
   ngxDocViewer: 'google',
 };
